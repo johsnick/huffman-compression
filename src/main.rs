aa5ff549bc2ffe84024b5915bdf2e3e74231c053
Nick Johs <johsnick@gmail.com>
2018-04-24 01:00:36 -0500

    add testing stuff

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ struct Elem {
   character: char,
 }
 
+struct CodeWord{
+  len: usize,
+  code: usize,
+  character: char
+}
+
 fn main() {
 
   let s =  match env::args().nth(1) {
@@ -35,36 +41,45 @@ fn main() {
   }
 
   let mut alphabet: Vec<Elem> = (0u8..255).zip(counts.iter())
-    .filter(|&(_, c)| *c != 0)
     .map(|(a, b)| Elem {count: *b, character: a as char} )
+    // .filter(|a| a.count != 0)
     .collect();
 
   alphabet.sort_by(|a, b| a.count.cmp(&b.count));
-  let mut vdata : Vec<_> = alphabet.iter().map(|e| e.count).collect::<Vec<_>>();
-  let data = &mut vdata;
-  println!("{:?}", data);
+  let mut lengths : Vec<_> = alphabet.iter().map(|e| e.count as usize).collect();
+  find_lengths(&mut lengths);
+  let mut code = len_to_codewords(&lengths, &alphabet);
+  code.sort_by(|a, b| a.character.cmp(&b.character));
+  let s : Vec<_>= code.iter().map(|c| c.character as usize).collect();
+  println!("{:?}", s);
 }
 
 
 #[test]
 fn test_find_lengths() {
-  let  data = &mut [2,2,2,2,200];
+  let  data = &mut [1,1,1,1,5];
   find_lengths(data);
-  assert_eq!(data, &mut [3,3,2,2,1]);
+  assert_eq!(data, &mut [3,3,3,3,1]);
 
   let data = &mut [1,1,2,2,4];
   find_lengths(data);
   assert_eq!(data, &mut [3,3,2,2,2]); 
 }
 
+#[test]
+fn hard_test_find_lengths() {
+  let data = &mut [1, 1, 2, 3, 3, 4, 5, 5, 5, 5, 5, 6, 6];
+  find_lengths(data);
+  assert_eq!(data, &mut [6,6,5,4,4,4,4,4,3,3,3,3,3]);
+}
 fn find_lengths(data: &mut [usize]) {
   let mut zi = 0;
   let mut li = 1;
   let mut hi = 2;
   data[0] += data[1];
 
-  while hi != data.len() {
-    if data[zi] >= data[hi] || data[zi + 1] >= data[hi] || zi == data.len() - 3 {
+  while zi != data.len() - 2 {
+    if hi < data.len() && (data[zi] >= data[hi] || data[zi + 1] >= data[hi] || zi == data.len() - 3) {
       if data.len() != hi + 1 && data[zi] >= data[hi + 1] {
         data[li] = data[hi] + data[hi + 1];
         hi += 2;
@@ -93,6 +108,8 @@ fn find_lengths(data: &mut [usize]) {
   }
   li -= 1;
   data[li] = 0;
+  println!("{:?}", data);
+  println!("{:?}", li);
   for i in (0..li).rev() {
     data[i] = data[data[i]] + 1;
   }
@@ -103,18 +120,35 @@ fn find_lengths(data: &mut [usize]) {
 
   let mut i : isize = data.len() as isize - 1;
   let mut depth = 1;
+  let mut taken = 0;
   while i >= 0 {
-    let total = depth << 1;
-    let found = total as isize - data.iter().filter(|&&e| e == depth).collect::<Vec<_>>().len() as isize;
-    for j in 0..found {
-      if j > i {
-        break;
-      }
+    let mut available = 2isize.pow(depth as u32) - taken;
+    available -= data.iter().filter(|&&e| e == depth).collect::<Vec<_>>().len() as isize;
 
+    for j in 0..available {
       data[(i - j as isize) as usize] = depth;
     }
 
-    i -= found;
+    i -= available;
     depth += 1;
+    taken += available;
+    taken *= 2;
   }
+}
+
+fn len_to_codewords(data: & [usize], elems: & [Elem]) -> Vec<CodeWord> {
+  let mut count = 0;
+  let mut last_index = 0;
+  let mut result = Vec::new();
+  for (&i, e) in data.iter().zip(elems).rev() {
+    if i != last_index && last_index != 0 {
+      count <<= i - last_index;
+    }
+    result.push(CodeWord{len: i, code: count, character: e.character});
+    count += 1;
+    last_index = i;
+  }
+
+  result.reverse();
+  result
 }
